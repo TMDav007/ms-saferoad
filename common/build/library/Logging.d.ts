249fd36497922f781f4afb053b54bdf139367ea8
Toluwalase <toluwalase@Toluwalases-MacBook-Air.local>
2023-02-25 20:06:13 +0000

    Update common package

--- common/build/library/Logging.d.ts
+++ common/build/library/Logging.d.ts
@@ -1,6 +1,7 @@
-export declare class Logging {
+declare class Logging {
     static log: (args: any) => (args: any) => void;
     static info: (args: any) => void;
     static warn: (args: any) => void;
     static error: (args: any) => void;
 }
+export { Logging };
