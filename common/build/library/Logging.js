9ae560bf868d677fb50676997b753699973d81ec
Toluwalase <toluwalase@Toluwalases-MacBook-Air.local>
2023-02-25 22:53:32 +0000

    Update common package

--- common/build/library/Logging.js
+++ common/build/library/Logging.js
@@ -1,13 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Logging = void 0;
-const { chalk } = require("chalk");
+const chalk_1 = __importDefault(require("chalk"));
 class Logging {
 }
 exports.Logging = Logging;
 _a = Logging;
 Logging.log = (args) => _a.info;
-Logging.info = (args) => console.log(chalk.blue(`[${new Date().toLocaleString()}] [INFO] `), typeof args === "string" ? chalk.blue(args) : args);
-Logging.warn = (args) => console.log(chalk.yellow(`[${new Date().toLocaleString()}] [WARN] `), typeof args === "string" ? chalk.yellowBright(args) : args);
-Logging.error = (args) => console.log(chalk.red(`[${new Date().toLocaleString()}] [ERROR] `), typeof args === "string" ? chalk.redBright(args) : args);
+Logging.info = (args) => console.log(chalk_1.default.blue(`[${new Date().toLocaleString()}] [INFO] `), typeof args === "string" ? chalk_1.default.blue(args) : args);
+Logging.warn = (args) => console.log(chalk_1.default.yellow(`[${new Date().toLocaleString()}] [WARN] `), typeof args === "string" ? chalk_1.default.yellowBright(args) : args);
+Logging.error = (args) => console.log(chalk_1.default.red(`[${new Date().toLocaleString()}] [ERROR] `), typeof args === "string" ? chalk_1.default.redBright(args) : args);
