baefed797243af8b7eed58936153298b66ba87eb
Toluwalase <toluwalase@Toluwalases-MacBook-Air.local>
2023-02-28 05:17:25 +0000

    Update common package

--- common/src/middlewares/error-middleware.ts
+++ common/src/middlewares/error-middleware.ts
@@ -1,35 +1,36 @@
 import { Response, Request, NextFunction, ErrorRequestHandler } from "express";
 import { StatusCodes } from "http-status-codes";
+import { Logging } from "../library/Logging";
 
 class AppError extends Error {
-	code: number;
+  code: number;
 
-	constructor(code: number, message: string) {
-		super();
-		this.code = code;
-		this.message = message;
-		this.stack =
-			(process.env.NODE_ENV as string) === "production" ? "" : this.stack;
-	}
+  constructor(code: number, message: string) {
+    super();
+    this.code = code;
+    this.message = message;
+    this.stack =
+      (process.env.NODE_ENV as string) === "production" ? "" : this.stack;
+  }
 }
 
 const errorHandler: ErrorRequestHandler = (
-	error: any | AppError,
-	req: Request,
-	res: Response,
-	next: NextFunction
+  error: any | AppError,
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-	const code = error.code || StatusCodes.INTERNAL_SERVER_ERROR;
-	let message = error.message;
-
-	if (!(error instanceof AppError)) {
-		message = "This problem is from our end, please try again";
-	}
-	return res.status(code).send({
-		message: message,
-		success: false,
-		data: null,
-	});
+  const code = error.code || StatusCodes.INTERNAL_SERVER_ERROR;
+  let message = error.message;
+  Logging.error(`Server error ${message}`);
+  if (!(error instanceof AppError)) {
+    message = "This problem is from our end, please try again";
+  }
+  return res.status(code).send({
+    message: message,
+    success: false,
+    data: null,
+  });
 };
 
 export { AppError, errorHandler };
